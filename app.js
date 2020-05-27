df367321053b69639b83eb48fa9fd4057b516551
hieu-ng <hieutnguyen06@gmail.com>
2020-05-27 21:12:18 +1000

    Migrate file from 'odin' repo

---
+++ app.js
@@ -0,0 +1,46 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.books');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = this.querySelector('[name=item]').value;
+    const item = {
+        text,
+        done: false,
+    };
+
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+}
+
+function populateList(books = [], booksList) {
+    booksList.innerHTML = books
+        .map((book, i) => {
+            return `
+        <li>
+          <input type="checkbox" data-index=${i} id="item${i}" ${
+                book.done ? 'checked' : ''
+            } />
+          <label for="item${i}">${book.text}</label>
+        </li>
+      `;
+        })
+        .join('');
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip this unless it's an input
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
